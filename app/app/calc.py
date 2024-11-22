61b16b6a51efd9a0ac71fa99d8cf333db1b93453
Umair Iqbal <148035536+umairiqbal98@users.noreply.github.com>
2024-11-22 22:39:09 +0500

    changes to settings.py docker-compose.yml and requirement.txt and dockerfile

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator app
+"""
+
+def add(x,y):
+    """Add two numbers"""
+    return x + y
+
+def subtract(x,y):
+    """Subtract two numbers"""
+    return x - y
---
+++ app/app/tests.py
@@ -0,0 +1,22 @@
+"""
+Simple Test for Calc Function
+"""
+
+from django.test import SimpleTestCase
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """Test for Calc Functions"""
+    def test_add_numbers(self):
+        """Test that two numbers are added together"""
+        self.assertEqual(calc.add(3, 8), 11)
+        self.assertEqual(calc.add(-3, 8), 5)
+        self.assertEqual(calc.add(-3, -8), -11)
+        self.assertEqual(calc.add(300, 800), 1100)
+
+    def test_subtract_numbers(self):
+        """Test that values are subtracted and returned"""
+        self.assertEqual(calc.subtract(5, 11), -6)
+        self.assertEqual(calc.subtract(11, 5), 6)
+        self.assertEqual(calc.subtract(-5, 11), -16)
+        self.assertEqual(calc.subtract(-11, 5), -16)
