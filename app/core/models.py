ce6cdb2e2410480eae8dabe2615e5b5e18ff4494
Umair Iqbal <148035536+umairiqbal98@users.noreply.github.com>
2024-12-08 22:47:06 +0500

    implemented custom user model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,35 @@
-# from django.db import models
+"""
+Database models for the core app.
+"""
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 
-# # Create your models here.
+class UserManager(BaseUserManager):
+    """Manager for user profiles"""
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create a new user profile"""
+        if not email:
+            raise ValueError('Users must have an email address')
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    # AbstractBaseUser: Base class for authenticating users. To use this class, you must set the USERNAME_FIELD attribute.
+    # PermissionsMixin: A mixin class that adds the fields and methods necessary to support Django's Group and Permission model using the ModelBackend.
+    """Custom user model that supports using email instead of username"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
