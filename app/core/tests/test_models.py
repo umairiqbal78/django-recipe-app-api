3537d5d0f7ef319e97cb687ec56f3f49b20f2a76
Umair Iqbal <148035536+umairiqbal98@users.noreply.github.com>
2024-12-09 00:53:21 +0500

    added user must have email address test

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -30,3 +30,8 @@ class ModelTests(TestCase):
         for email, expected_email in sample_emails:
             user = get_user_model().objects.create_user(email, 'sample123')
             self.assertEqual(user.email, expected_email)
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'sample123')
